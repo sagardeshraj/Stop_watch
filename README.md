332d0b0bb9927f26b79b9d5be85ea176a9ab13c5
sagardeshraj <saagardeshraj14@gmail.com>
2023-03-24 01:36:48 +0530

    commited

---
+++ README.md
@@ -0,0 +1,7 @@
+# Stopwatch
+
+A simple stopwatch app written with vanilla css and javascript, no frameworks/libraries whatsoever.
+
+## How to run the app
+
+All you need to do is to download or clone the repository, then open the index.html file in your browser.
---
+++ app.js
@@ -0,0 +1,134 @@
+(function () {
+  // Start button
+  var startButton = document.querySelector("#js--start");
+  // Stop button
+  var stopButton = document.querySelector("#js--stop");
+  // Reset button
+  var resetButton = document.querySelector("#js--reset");
+  // Stopwatch, the element which will show the time
+  var stopwatch = document.querySelector("#js--stopwatch");
+
+  // Variable to use for the stopwatch time
+  var time = 0; 
+
+  // Show an element on the screen
+  function hide(element) {
+    element.classList.add("u-display-none");
+  }
+
+  // Hide an element on the screen
+  function show(element) {
+    element.classList.remove("u-display-none");
+  }
+
+  // Make an element disabled or enabled
+  function disable(element, boolean) {
+    if (boolean) {
+      element.disabled = true;
+    } else {
+      element.disabled = false;
+    }
+  }
+
+  // Format and display the time on the screen
+  function display(time) {
+    var centiseconds = Math.floor((time % 1000) / 10);
+    var seconds = Math.floor(time / 1000) % 60;
+    var minutes = Math.floor(Math.floor(time / 1000) / 60);
+
+    // Format centisecond before shown on the screen
+    if (centiseconds < 10) {
+      // If centiseconds is less than 10, add a zero before the centiseconds in string
+      centiseconds = "0" + centiseconds.toString();
+    } else {
+      // Otherwise just show the centiseconds
+      centiseconds = centiseconds.toString();
+    }
+
+    // Format seconds before shown on the screen
+    if (seconds < 10) {
+      // If seconds is less than 10, add a zero before the seconds in string
+      seconds = "0" + seconds.toString();
+    } else {
+      // Otherwise just show the seconds
+      seconds = seconds.toString();
+    }
+
+    // Format minutes before shown on the screen
+    if (minutes < 10) {
+      // If minutes is less than 10, add a zero before the minutes in string
+      minutes = "0" + minutes.toString();
+    } else {
+      // Otherwise just show the minutes
+      minutes = minutes.toString();
+    }
+
+    // Update the UI
+    stopwatch.innerHTML = minutes + ":" + seconds + "." + centiseconds;
+  }
+
+  display(0);
+
+  var stopwatchTimerId;
+
+  // When the start button gets clicked.
+  // Users can only see this button whenever the stopwatch is NOT running
+  startButton.addEventListener("click", function () {
+    hide(startButton);
+    show(stopButton);
+    disable(resetButton, true);
+
+    var startTime = new Date(),
+      currentTime,
+      actualTime,
+      toAdd = 0;
+
+    stopwatchTimerId = setInterval(function () {
+      currentTime = new Date();
+      actualTime = currentTime - startTime + toAdd;
+
+      // Increment our time variable by 10ms (1 centisecond) at each interval
+      time = time + 10;
+
+      if (actualTime - time !== 0) {
+        if (actualTime - time < -10) {
+          // We'll run this after we had a pause
+          toAdd = time - actualTime;
+        } else {
+          time = actualTime;
+        }
+      }
+
+      display(time);
+    }, 10);
+  });
+
+  // When the stop button gets clicked.
+  // Users can only see this button while the stopwatch is running
+  stopButton.addEventListener("click", function () {
+    hide(stopButton);
+    show(startButton);
+    disable(resetButton, false);
+
+    /* 
+      // This is how we can terminate a worker:
+      if (timer) {
+        timer.terminate();
+        timer = null;
+      }
+    */
+
+    if (stopwatchTimerId) clearInterval(stopwatchTimerId);
+  });
+
+  // When the reset button gets clicked.
+  // Users can only see this button if the stopwatch is stopped and has a value
+  resetButton.addEventListener("click", function () {
+    disable(resetButton, true);
+
+    // Reset the timer value
+    time = 0;
+
+    display(0);
+  });
+})();
